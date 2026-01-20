8b45d2c1ddcb5f52629586f2bc42d8f312d80997
thienchiedu <thienchi2109@gmail.com>
2026-01-20 19:59:29 +0700

    feat(ui): update favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
   keywords: ["e-commerce", "shopping", "vietnam", "household"],
   icons: {
     icon: [
-      { url: "/favicon_io/favicon.ico" },
+
       { url: "/favicon_io/favicon-16x16.png", sizes: "16x16", type: "image/png" },
       { url: "/favicon_io/favicon-32x32.png", sizes: "32x32", type: "image/png" },
       { url: "/favicon_io/android-chrome-192x192.png", sizes: "192x192", type: "image/png" },
